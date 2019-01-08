343e3f21868ae73a94d78562bd9e087dcb33d180
Mykola Misiura <nicolas.misiura@gmail.com>
2019-01-08 16:18:25 +0200

    Changed installation steps

--- install_monero.sh
+++ install_monero.sh
@@ -12,11 +12,6 @@ MONERO_CORE_URL="https://github.com/fotolockr/monero-gui.git"
 MONERO_CORE_DIR_PATH="$EXTERNAL_DIR_PATH/monero-gui"
 MONERO_URL="https://github.com/fotolockr/monero.git"
 MONERO_DIR_PATH="$MONERO_CORE_DIR_PATH/monero"
-SODIUM_PATH="$EXTERNAL_SOURCES_DIR_PATH/libsodium"
-SODIUM_URL="https://github.com/jedisct1/libsodium.git"
-SODIUM_LIBRARY_PATH="$SODIUM_PATH/libsodium-ios/lib/libsodium.a"
-SODIUM_INCLUDE_PATH="$SODIUM_PATH/libsodium-ios/include"
-
 
 if [ -z "$EXTERNAL_LIBS_PATH"]
 then
@@ -25,32 +20,7 @@ fi
 
 EXTERNAL_MONERO_LIB_PATH="$EXTERNAL_LIBS_PATH/monero/libs"
 EXTERNAL_MONERO_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/monero/include"
-EXTERNAL_SODIUM_LIB_PATH="$EXTERNAL_LIBS_PATH/sodium/lib"
-EXTERNAL_SODIUM_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/sodium/include"
 
-echo "============================ SODIUM ============================"
-echo "Cloning SODIUM from - $SODIUM_URL"
-git clone $SODIUM_URL $SODIUM_PATH --branch stable
-cd $SODIUM_PATH
-./dist-build/ios.sh
-
-#copy to libs directory
-mkdir -p $EXTERNAL_SODIUM_LIB_PATH
-mkdir -p $EXTERNAL_SODIUM_INCLUDE_PATH
-mv $SODIUM_PATH/libsodium-ios/lib/* $EXTERNAL_SODIUM_LIB_PATH/
-mv $SODIUM_PATH/libsodium-ios/include/* $EXTERNAL_SODIUM_INCLUDE_PATH/
-
-cd ../..
-
-echo "============================ Monero-gui ============================"
-
-echo "Cloning monero-gui from - $MONERO_CORE_URL"
-git clone -b build $MONERO_CORE_URL $MONERO_CORE_DIR_PATH
-echo "Cloning monero from - $MONERO_URL to - $MONERO_DIR_PATH"
-git clone -b build $MONERO_URL $MONERO_DIR_PATH
-cd $MONERO_DIR_PATH
-git submodule init && git submodule update
-cd ..
 echo "Export Boost vars"
 export BOOST_LIBRARYDIR="${EXTERNAL_UTILS_DIR_PATH}/ofxiOSBoost/build/ios/prefix/lib"
 export BOOST_LIBRARYDIR_x86_64="${EXTERNAL_UTILS_DIR_PATH}/ofxiOSBoost/build/libs/boost/lib/x86_64"
