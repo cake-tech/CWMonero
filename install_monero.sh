54643f6b4df0a37cab1c0bd07c901515615c21f5
Mykola Misiura <nicolas.misiura@gmail.com>
2019-01-08 09:22:16 +0200

    Changed dependencies installation scripts

--- install_monero.sh
+++ install_monero.sh
@@ -3,24 +3,43 @@
 SOURCE_DIR=`pwd`
 EXTERNAL_DIR_PATH="$SOURCE_DIR/External"
 EXTERNAL_UTILS_DIR_PATH="`pwd`/../SharedExternal"
+EXTERNAL_SOURCES_DIR_PATH="$SOURCE_DIR/../SharedExternal"
 BOOST_URL="https://github.com/fotolockr/ofxiOSBoost.git"
-BOOST_DIR_PATH="$EXTERNAL_UTILS_DIR_PATH/ofxiOSBoost"
+BOOST_DIR_PATH="$EXTERNAL_SOURCES_DIR_PATH/ofxiOSBoost"
 OPEN_SSL_URL="https://github.com/x2on/OpenSSL-for-iPhone.git"
-OPEN_SSL_DIR_PATH="$EXTERNAL_UTILS_DIR_PATH/OpenSSL"
+OPEN_SSL_DIR_PATH="$EXTERNAL_SOURCES_DIR_PATH/OpenSSL"
 MONERO_CORE_URL="https://github.com/fotolockr/monero-gui.git"
 MONERO_CORE_DIR_PATH="$EXTERNAL_DIR_PATH/monero-gui"
 MONERO_URL="https://github.com/fotolockr/monero.git"
 MONERO_DIR_PATH="$MONERO_CORE_DIR_PATH/monero"
-SODIUM_PATH="$EXTERNAL_UTILS_DIR_PATH/libsodium"
+SODIUM_PATH="$EXTERNAL_SOURCES_DIR_PATH/libsodium"
 SODIUM_URL="https://github.com/jedisct1/libsodium.git"
 SODIUM_LIBRARY_PATH="$SODIUM_PATH/libsodium-ios/lib/libsodium.a"
 SODIUM_INCLUDE_PATH="$SODIUM_PATH/libsodium-ios/include"
+
+
+if [ -z "$EXTERNAL_LIBS_PATH"]
+then
+  EXTERNAL_LIBS_PATH="$EXTERNAL_UTILS_DIR_PATH/libs"
+fi
+
+EXTERNAL_MONERO_LIB_PATH="$EXTERNAL_LIBS_PATH/monero/libs"
+EXTERNAL_MONERO_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/monero/include"
+EXTERNAL_SODIUM_LIB_PATH="$EXTERNAL_LIBS_PATH/sodium/lib"
+EXTERNAL_SODIUM_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/sodium/include"
 
 echo "============================ SODIUM ============================"
 echo "Cloning SODIUM from - $SODIUM_URL"
 git clone $SODIUM_URL $SODIUM_PATH --branch stable
 cd $SODIUM_PATH
 ./dist-build/ios.sh
+
+#copy to libs directory
+mkdir -p $EXTERNAL_SODIUM_LIB_PATH
+mkdir -p $EXTERNAL_SODIUM_INCLUDE_PATH
+mv $SODIUM_PATH/libsodium-ios/lib/* $EXTERNAL_SODIUM_LIB_PATH/
+mv $SODIUM_PATH/libsodium-ios/include/* $EXTERNAL_SODIUM_INCLUDE_PATH/
+
 cd ../..
 
 echo "============================ Monero-gui ============================"
@@ -43,3 +62,19 @@ export SODIUM_LIBRARY=$SODIUM_LIBRARY_PATH
 export SODIUM_INCLUDE=$SODIUM_INCLUDE_PATH
 mkdir -p monero/build
 ./ios_get_libwallet.api.sh
+echo "Copy dependencies"
+mkdir -p $EXTERNAL_MONERO_LIB_PATH
+mkdir -p $EXTERNAL_MONERO_LIB_PATH/lib-ios
+mkdir -p $EXTERNAL_MONERO_LIB_PATH/lib
+mkdir -p $EXTERNAL_MONERO_LIB_PATH/lib-x86_64
+mkdir -p $EXTERNAL_MONERO_INCLUDE_PATH
+mkdir -p $EXTERNAL_MONERO_INCLUDE_PATH/src
+mkdir -p $EXTERNAL_MONERO_INCLUDE_PATH/external
+mkdir -p $EXTERNAL_MONERO_INCLUDE_PATH/contrib
+
+mv $MONERO_DIR_PATH/lib-ios/* $EXTERNAL_MONERO_LIB_PATH/lib-ios/
+mv $MONERO_DIR_PATH/lib/* $EXTERNAL_MONERO_LIB_PATH/lib/
+mv $MONERO_DIR_PATH/lib-x86_64/* $EXTERNAL_MONERO_LIB_PATH/lib-x86_64/
+mv $MONERO_DIR_PATH/src/* $EXTERNAL_MONERO_INCLUDE_PATH/src
+mv $MONERO_DIR_PATH/external/* $EXTERNAL_MONERO_INCLUDE_PATH/external
+mv $MONERO_DIR_PATH/contrib/* $EXTERNAL_MONERO_INCLUDE_PATH/contrib
--- install_utils_deps.sh
+++ install_utils_deps.sh
@@ -2,12 +2,23 @@
 
 SOURCE_DIR=`pwd`
 EXTERNAL_DIR_PATH="$SOURCE_DIR/../SharedExternal"
+EXTERNAL_SOURCES_DIR_PATH="$SOURCE_DIR/../SharedExternal"
 BOOST_URL="https://github.com/fotolockr/ofxiOSBoost.git"
-BOOST_DIR_PATH="$EXTERNAL_DIR_PATH/ofxiOSBoost"
+BOOST_DIR_PATH="$EXTERNAL_SOURCES_DIR_PATH/ofxiOSBoost"
 OPEN_SSL_URL="https://github.com/x2on/OpenSSL-for-iPhone.git"
-OPEN_SSL_DIR_PATH="$EXTERNAL_DIR_PATH/OpenSSL"
+OPEN_SSL_DIR_PATH="$EXTERNAL_SOURCES_DIR_PATH/OpenSSL"
 LMDB_DIR_URL="https://github.com/LMDB/lmdb.git"
-LMDB_DIR_PATH="$EXTERNAL_DIR_PATH/lmdb/Sources"
+LMDB_DIR_PATH="$EXTERNAL_SOURCES_DIR_PATH/lmdb/Sources"
+
+if [ -z "$EXTERNAL_LIBS_PATH"]
+then
+  EXTERNAL_LIBS_PATH="$EXTERNAL_DIR_PATH/libs"
+fi
+
+EXTERNAL_BOOST_LIB_PATH="$EXTERNAL_LIBS_PATH/boost/lib"
+EXTERNAL_BOOST_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/boost/include"
+EXTERNAL_OPENSSL_LIB_PATH="$EXTERNAL_LIBS_PATH/OpenSSL/lib"
+EXTERNAL_OPENSSL_INCLUDE_PATH="$EXTERNAL_LIBS_PATH/OpenSSL/include"
 
 echo "============================ Init external libs ============================"
 mkdir -p $EXTERNAL_DIR_PATH
@@ -19,6 +30,13 @@ git clone -b build $BOOST_URL $BOOST_DIR_PATH
 cd $BOOST_DIR_PATH/scripts/
 export BOOST_LIBS="random regex graph random chrono thread signals filesystem system date_time locale serialization program_options"
 ./build-libc++
+
+#copy to libs directory
+mkdir -p $EXTERNAL_BOOST_LIB_PATH
+mkdir -p $EXTERNAL_BOOST_INCLUDE_PATH
+mv $BOOST_DIR_PATH/libs/boost/ios/* $EXTERNAL_BOOST_LIB_PATH/
+mv $BOOST_DIR_PATH/libs/boost/include/* $EXTERNAL_BOOST_INCLUDE_PATH/
+
 cd $SOURCE_DIR
 
 echo "============================ OpenSSL ============================"
@@ -26,7 +44,14 @@ echo "============================ OpenSSL ============================"
 echo "Cloning Open SSL from - $OPEN_SSL_URL"
 git clone $OPEN_SSL_URL $OPEN_SSL_DIR_PATH
 cd $OPEN_SSL_DIR_PATH
-./build-libssl.sh --version=1.0.2j
+./build-libssl.sh --version=1.0.2j --archs="x86_64 arm64 armv7s armv7" --targets="ios-sim-cross-x86_64 ios64-cross-arm64 ios-cross-armv7s ios-cross-armv7"
+
+#copy to libs directory
+mkdir -p $EXTERNAL_OPENSSL_LIB_PATH
+mkdir -p $EXTERNAL_OPENSSL_INCLUDE_PATH
+mv $OPEN_SSL_DIR_PATH/lib/* $EXTERNAL_OPENSSL_LIB_PATH/
+mv $OPEN_SSL_DIR_PATH/include/* $EXTERNAL_OPENSSL_INCLUDE_PATH/
+
 cd $SOURCE_DIR
 
 echo "============================ LMDB ============================"
@@ -34,4 +59,5 @@ echo "Cloning lmdb from - $LMDB_DIR_URL"
 git clone $LMDB_DIR_URL $LMDB_DIR_PATH
 cd $LMDB_DIR_PATH
 git checkout b9495245b4b96ad6698849e1c1c816c346f27c3c
+
 cd $SOURCE_DIR
